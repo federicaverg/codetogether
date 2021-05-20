d5866b610e6de4b99d3897e3c0a43512de71bd50
Giorgio Marini <gmi20001@student.mdh.se>
2021-05-20 13:56:14 +0200

    fixed tabs

--- src/Components/DisplayExercise.js
+++ src/Components/DisplayExercise.js
@@ -1,5 +1,6 @@
 import React, {PureComponent} from 'react';
-import {Typography, Layout,Row, Col, Tabs, Skeleton, Card, Comment, Tooltip, List} from 'antd';
+import {Typography, Layout,Row, Col, Tabs, Dropdown, Menu, Button, Card, Comment, Tooltip, List} from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 import moment from 'moment';
 import axios from 'axios';
 
@@ -10,7 +11,8 @@ const {Content} = Layout;
 
 const { TabPane } = Tabs;
 
-const data = [
+// Hard-coded comments
+const comments = [
   {
     //actions: [<span key="comment-list-reply-to-0">Reply to</span>],
     author: 'John Doe',
@@ -43,138 +45,67 @@ const data = [
   },
 ];  
 
-  const exercise = [];
-  
-
-const initialPanes = [
-    { title: 'Source code', content: "", key: '1', closable: false, },
-    //{ title: 'V1', content: 'Version', key: '2' },
-  ];
+const exercise = [];
 
 export default class DisplayExercise extends PureComponent {
   
-
   constructor(props){
     super(props);
 
-    
-
     this.state = {
-      activeKey: initialPanes[0].key,
-      panes: initialPanes,
-      cont: initialPanes[0].content,
+      cont: ["ciao"],
       prova: props.match
       };
 
-    
     console.log(this.state.prova);
   }
-  newTabIndex = 0;
-  
 
-  // componentDidMount() {
-  //   axios.get(`http://localhost:5000/exercises/6089303edca19abd5e2e8cc0`)
-  //   .then(response => {
-  //     console.log(response.data)
-  //     this.setState({cont: response.data.code })
-  //     console.log(this.state.cont)
-  //   })
-  //   .catch((error) => { console.log(error);})
-
-  // }
-
-  // ID = 6089303edca19abd5e2e8cc0
-
-  async componentDidMount() {
-    console.log("HELLO");
-    console.log(this.state.cont);
-    try {
-      console.log(this.state.prova.params.id);
-      const response = await axios.get(`http://localhost:5000/exercises/title/${this.state.prova.params.id}`);
-      console.log(response);
-      this.setState({cont: response.data })
+  componentDidMount() {
+    axios.get(`http://localhost:5000/exercises/title/${this.state.prova.params.id}`)
+    .then(response => {
+      console.log(response)
+      this.setState({cont: response.data.code })
       console.log(this.state.cont)
-    } catch (error) {
-      console.error(error);
-    }
+    })
+    .catch((error) => { console.log(error);})
   }
 
-    onChange = activeKey => {
-        this.setState({ activeKey });
-    };
-
-    onEdit = (targetKey, action) => {
-        this[action](targetKey);
-    };
-
-    add = () => {
-        const { panes } = this.state;
-        const activeKey = `newTab${this.newTabIndex++}`;
-        const newPanes = [...panes];
-        newPanes.push({ title: 'New Tab', content: 'Content of new Tab', key: activeKey });
-        this.setState({
-        panes: newPanes,
-        activeKey,
-        });
-    };
-
-    remove = targetKey => {
-        const { panes, activeKey } = this.state;
-        let newActiveKey = activeKey;
-        let lastIndex;
-        panes.forEach((pane, i) => {
-        if (pane.key === targetKey) {
-            lastIndex = i - 1;
-        }
-        });
-        const newPanes = panes.filter(pane => pane.key !== targetKey);
-        if (newPanes.length && newActiveKey === targetKey) {
-        if (lastIndex >= 0) {
-            newActiveKey = newPanes[lastIndex].key;
-        } else {
-            newActiveKey = newPanes[0].key;
-        }
-        }
-        this.setState({
-        panes: newPanes,
-        activeKey: newActiveKey,
-        });
-    };
-
     render() {
-        const { panes, activeKey, cont } = this.state;
         return(
             <div className="display-exercise">
                 <Content style={{ padding: '0 50px', marginTop:55}}>
-
                 <Row justify="space-around">
 
-                <Col span={15}><Tabs
-                type="editable-card"
-                onChange={this.onChange}
-                activeKey={activeKey}
-                onEdit={this.onEdit}>
-                {panes.map(pane => ( <TabPane tab={pane.title} key={pane.key} closable={pane.closable}>
-                    <Container codice={"culo"}/></TabPane>))}
-                </Tabs></Col>
+                <Col span={15}>
+                  
+                <Tabs defaultActiveKey="1">
+                      <TabPane tab="SOURCE CODE" key="1">
+                      <Container codice={"this.state.cont"}/>
+                      </TabPane>
+                    </Tabs></Col>
 
-
-
-
-
-
-
+                    <Dropdown overlay={
+                      <Menu>
+                            <Menu.Item key="display">
+                              Version 1
+                            </Menu.Item>
+                            <Menu.Item key="mode">
+                              Version 8
+                            </Menu.Item>
+                        </Menu>}>
+                        <Button icon={<PlusOutlined />} />
+                      </Dropdown>
 
                 <Col span={6}><Card title={this.state.cont.title} >
-                    <p><i>this.state.cont.</i></p>
+                    <p><i>{this.state.cont.date}</i></p>
                     <p>{this.state.cont.description}</p>
                     </Card>
 
                     <List
                     className="comment-list"
-                    header={`${data.length} replies`}
+                    header={`${comments.length} replies`}
                     itemLayout="horizontal"
-                    dataSource={data}
+                    dataSource={comments}
                     renderItem={item => (
                     <li>
                         <Comment
