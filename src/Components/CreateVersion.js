4d188239598ce17618b5aaa0b8fa087ae84e038c
federicaverg <federica.vergari@gmail.com>
2021-05-24 11:30:28 +0200

    Access values of version creation form

--- src/Components/CreateVersion.js
+++ src/Components/CreateVersion.js
@@ -14,44 +14,31 @@ const layout = {
 };
   
 const onFinish = (values) => {
-    console.log(values);};
-
-
-function handleChange(value) {
-        console.log(`selected ${value}`);
-      }
+    console.log(values);
+    //console.log(values.exercise);
+    //console.log(values.description);
+    //console.log(values.code);
+  };
   
 export default class CreateVersion extends PureComponent {
-    state = {
-        value: '',
-      };
     
-      stateA = {
-        inputValue: 1,
-      };
-    
-      onChange = ({ target: { value } }) => {
-        this.setState({ value });
-      };
       render() {
-        const { value } = this.state;
         return (
           <div className="create-source-code" style={{ padding: '0 50px', marginTop:80,}}>
           <Form {...layout} name="nest-messages" onFinish={onFinish} >
-          <Form.Item name="Exercise" label={<label style={{textTransform:'uppercase',letterSpacing:'2px', fontSize:'14px'}}>Exercise</label>} rules={[{required:true}]} >
-            <Select style={{ width: 200 }} onChange={handleChange}>
-            <Option value="1">Exercise 1</Option>
+          <Form.Item name="exercise" label={<label style={{textTransform:'uppercase',letterSpacing:'2px', fontSize:'14px'}}>Exercise</label>} rules={[{required:true}]} >
+            <Select style={{ width: 200 }}>
+            <Option value="1" /*exercise._id*/>Exercise 1</Option>
             <Option value="2">Exercise A*</Option>
             </Select>
           </Form.Item>
 
-          <Form.Item label={<label style={{textTransform:'uppercase',letterSpacing:'2px', fontSize:'14px'}}>Description</label>} >
+          <Form.Item name="description" label={<label style={{textTransform:'uppercase',letterSpacing:'2px', fontSize:'14px'}}>Description</label>} >
         <TextArea />
         </Form.Item>
 
-        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 5 }} rules={[{required: true}]}>
+        <Form.Item name="code" wrapperCol={{ ...layout.wrapperCol, offset: 5 }} rules={[{required: true}]}>
         <TextArea
-          value={value}
           onChange={this.onChange}
           placeholder="</>"
           autoSize={{ minRows: 5, maxRows: 15 }}
