19efc7f5abf7872e98172b2a3bd61fae36f3d1c0
Giorgio Marini <gmi20001@student.mdh.se>
2021-06-08 00:08:34 +0200

    Codejar added

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,29 +1,15 @@
-import React, { useState, useEffect } from "react";
 import Editor from "./Editor";
 
-// var code = codeWrong.replace(/\s/g, '');
 
-// function hex2a(hexx) {
-//     var hex = hexx.toString();
-//     var code = '';
-//     for (var i = 0; i < hex.length; i += 2)
-//         code += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-// return code;}
-
-// var codeRight = hex2a(code);
 
 export default function Container(props) {
-  const [value, setValue] = useState("");
-
-  useEffect(() => {
-    console.log(value);
-  }, [value]);
+  
 
   // reload browser after change
   return (
     <Editor
       initialValue={props.codice}
-      onChange={value => setValue(value)}
+      //onChange={value => setValue(value)}
     />
   );
 }
--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -1,40 +1,63 @@
-import React, { useEffect } from "react";
-import Yace from "yace";
+import React, { useState, useEffect } from "react";
 import Prism from "prismjs";
-import { highlight } from "mdhl";
+//import { highlight } from "mdhl";
+import { ReactCodeJar, useCodeJar } from "react-codejar";
+
 
 import "mdhl/mdhl.css";
 import "./prism.css";
 import "../Styles/style-container.css"
 
 
-function highlighter(value) {
+/*unction highlighter(value) {
     return Prism.highlight(value, Prism.languages.java, "java");
-}
+}*/
 
 export default function App({ initialValue, onChange, key }) {
+  const [value, setValue] = useState(initialValue);
+
+  
   useEffect(() => {
-    const editor = new Yace("#editor", {
-      lineNumbers: true,
-      value: initialValue,
-      styles: {
-        fontSize: "20px"
-      },
-      highlighter,
-      lineNumbers:true
-    });
+    console.log(value);
 
-    editor.onUpdate(onChange);
+  }, [value]);
 
-    return () => {
-      editor.root.innerHTML = "";
-      editor.destroy();
-    };
+  useEffect(() => {
+  //   const editor = new Yace("#editor", {
+  //     lineNumbers: true,
+  //     value: initialValue,
+  //     styles: {
+  //       fontSize: "20px"
+  //     },
+  //     highlighter,
+  //     lineNumbers:true
+  //   });
+
+  //   editor.onUpdate(setValue);
+
+  //   return () => {
+  //     editor.root.innerHTML = "";
+  //     editor.destroy();
+  //   };
   }, []); // eslint-disable-line
 
+  const highlight = editor => {
+    console.log(editor.textContent);
+    editor.innerHTML = Prism.highlight(editor.textContent, Prism.languages.java, "java");
+   
+  };
+
   return (
+    <ReactCodeJar
+      code={value} // Initial code value
+      onUpdate={setValue} // Update the text
+      highlight={highlight} // Highlight function, receive the editor
+      lineNumbers={true} // Show line numbers
+    />
+
+    /*
     <div className="wrap" key={key}>
       <div id="editor" />
-    </div>
+    </div>*/
   );
 }
